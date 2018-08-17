807b497a98bebabfac0b62be011c4525f57fa0c6
platylm <n_nareenart@kkumail.com>
2018-08-17 17:05:26 +0700

    [Created]:function test XML to JSON

--- src/model/model_test.go
+++ src/model/model_test.go
@@ -1,1 +1,23 @@
 package model_test
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
+	"model"
+	"testing"
+)
+
+func Test_XMLToJSON_Should_Be_JSON(t *testing.T) {
+	expected, _ := ioutil.ReadFile("./countrycodeinfo.json")
+	xmlfile, _ := ioutil.ReadFile("./result.xml")
+	var holidaysAvailableResult model.HolidaysAvailableResult
+
+	xml.Unmarshal(xmlfile, &holidaysAvailableResult)
+	holidayInfo := holidaysAvailableResult.ToJSON()
+	actual, _ := json.Marshal(holidayInfo)
+	if string(expected) != string(actual) {
+		t.Errorf("expected \n'%s' but got it \n'%s'", expected, actual)
+	}
+
+}

726f52c2100b5bdc3131653d8d039e8fe1a0f6eb
platylm <n_nareenart@kkumail.com>
2018-08-20 16:33:37 +0700

    [Edited]:เพิ่ม environment เข้ามาในการ run

--- src/holiday/main/main.go
+++ src/holiday/main/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"encoding/json"
 	"holiday/api"
 	"holiday/log"
 	"holiday/route"
 	"holiday/service"
+	"io/ioutil"
 	"os"
 	"time"
 )
 
+type Config struct {
+	HolidayWebServiceURL string        `json:"holidayWebServiceURL"`
+	TimeoutDuration      time.Duration `json:"timeoutDuration"`
+	Port                 string        `json:"port"`
+}
+
 func main() {
+	environment := os.Getenv("ENV") //development, production
+	if environment == "" {
+		environment = "development"
+	}
+	file, _ := ioutil.ReadFile("./configs/" + environment + ".json")
+	var config Config
+	json.Unmarshal(file, &config)
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "3000"
+		port = config.Port
 	}
+
 	timeoutSecond, err := time.ParseDuration(os.Getenv("TIMEOUT"))
 	if err != nil {
-		timeoutSecond = 30
+		timeoutSecond = config.TimeoutDuration
 	}
 	timeoutDuration := timeoutSecond * time.Second
 	holidayWebServiceURL := os.Getenv("HOLIDAY_WEBSERVICE_URL")
 	if holidayWebServiceURL == "" {
-		holidayWebServiceURL = "http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl"
+		holidayWebServiceURL = config.HolidayWebServiceURL
 	}
 
 	logger := log.LoggerMongo{}

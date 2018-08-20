ce3b405bf4ef82088b64574ebf08e8fb1737045a
ployploy <chonnikan.t@kkumail.com>
2018-08-20 12:49:00 +0700

    [Edited]: เรียกใช้งาน  logger และเขียน test โดยการใช้ mockLogger

--- src/service/service.go
+++ src/service/service.go
@@ -4,8 +4,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/xml"
+	"holiday/log"
+	"holiday/model"
 	"io/ioutil"
-	"model"
 	"net/http"
 	"time"
 )
@@ -13,11 +14,16 @@ import (
 const timeoutDuration = 30 * time.Second
 const holidayWebServiceURL = "http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl"
 
-func SendToHolidayWebService(countryCodeInfo model.CountryCodeInfo) (model.HolidayInfo, error) {
+type HolidayService struct {
+	Logger log.Logger
+}
+
+func (hs HolidayService) SendToHolidayWebService(countryCodeInfo model.CountryCodeInfo) (model.HolidayInfo, error) {
 	message := countryCodeInfo.ToHolidayAvailableMessage()
 	ctx, _ := context.WithTimeout(context.Background(), timeoutDuration)
 	XML, err := xml.Marshal(message)
 	if err != nil {
+		hs.Logger.Error(err.Error())
 		return model.HolidayInfo{}, err
 	}
 	request, _ := http.NewRequest("POST", holidayWebServiceURL, bytes.NewBuffer(XML))
@@ -25,13 +31,14 @@ func SendToHolidayWebService(countryCodeInfo model.CountryCodeInfo) (model.Holid
 	request = request.WithContext(ctx)
 	response, err := http.DefaultClient.Do(request)
 	if err != nil {
+		hs.Logger.Error(err.Error())
 		return model.HolidayInfo{}, err
 	}
 	var holidaysAvailableResult model.HolidaysAvailableResult
 
 	data, _ := ioutil.ReadAll(response.Body)
 	xml.Unmarshal(data, &holidaysAvailableResult)
-
+	hs.Logger.Info("SUCCESS")
 	return holidaysAvailableResult.ToHolidayInfo(), nil
 
 }
--- src/service/service_test.go
+++ src/service/service_test.go
@@ -1,8 +1,8 @@
 package service_test
 
 import (
-	"model"
-	. "service"
+	"holiday/model"
+	. "holiday/service"
 	"testing"
 )
 
@@ -11,9 +11,17 @@ func Test_SendToHolidayWebService_Input_Canada_Should_Be_48_Days(t *testing.T) {
 		CountryCode: "Canada",
 	}
 	expected := 48
-	actual, _ := SendToHolidayWebService(countryCode)
+	logger := mockLogger{}
+	holidayService := HolidayService{
+		Logger: &logger,
+	}
+	actual, _ := holidayService.SendToHolidayWebService(countryCode)
+
 	if expected != len(actual.Holidays) {
 		t.Errorf("expected %d but got it %d", expected, len(actual.Holidays))
+	}
+	if logger.Success != 1 {
+		t.Errorf("expected logger call %d but got it %d", 1, logger.Success)
 	}
 
 }

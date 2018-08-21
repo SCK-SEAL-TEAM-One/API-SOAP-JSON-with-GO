3c25a34a8120f499b4691b3100298f77c24bbb88
platylm <n_nareenart@kkumail.com>
2018-08-21 10:55:46 +0700

    [Created]:function Configuration

---
+++ src/holiday/config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type Config struct {
+	HolidayWebServiceURL string        `json:"holidayWebServiceURL"`
+	TimeoutDuration      time.Duration `json:"timeoutDuration"`
+	Port                 string        `json:"port"`
+	MongoURL             string        `json:"mongoURL"`
+}
+
+func Configuration() (Config, error) {
+	var config Config
+	var environment string
+
+	if os.Getenv("ENV") == "" {
+		environment = "development"
+	}
+
+	configFile, err := ioutil.ReadFile("./configs/" + environment + ".json")
+	if err != nil {
+		return config, err
+	}
+
+	err = json.Unmarshal(configFile, &config)
+	if err != nil {
+		return config, err
+	}
+
+	if os.Getenv("TIMEOUT") != "" {
+		config.TimeoutDuration, _ = time.ParseDuration(os.Getenv("TIMEOUT"))
+	}
+
+	if os.Getenv("PORT") != "" {
+		config.Port = os.Getenv("PORT")
+	}
+
+	holidayWebserviceURL := os.Getenv("HOLIDAY_WEBSERVICE_URL")
+	if holidayWebserviceURL != "" {
+		config.HolidayWebServiceURL = holidayWebserviceURL
+	}
+
+	return config, nil
+}
